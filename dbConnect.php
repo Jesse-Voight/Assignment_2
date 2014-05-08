b9d1cbd1bafda3919a9f837beb09ca9b4ae3f04c
Jesse-Voight <24051java1@gmail.com>
2014-05-08 15:57:36 +1200

    Fixed issues with creating the database and table schema and made the messages for creating the database and schema a bit more tidy.

--- dbConnect.php
+++ dbConnect.php
@@ -7,50 +7,44 @@ $databaseName = "notebook";
 
 $connection = mysql_connect("localhost", "assignment2", "winning") or die("Database connection to MySql server could not be established");
 
-$dbCheck = mysql_select_db($databaseName, $connection);
+$status = mysql_select_db($databaseName, $connection);
 
-if (!$dbCheck) {
+if (!$status) {
     $createDB = mysql_query("CREATE DATABASE $databaseName", $connection);
     if ($createDB) {
-        print("Created initial database $databaseName");
-        $dbCheck = mysql_select_db($databaseName, $connection);
-        if (!$dbCheck) {
+        print("Created initial database $databaseName. <br/>");
+        $status = mysql_select_db($databaseName, $connection);
+        if (!$status) {
             die("Couldnt open $databaseName - fatal error" . mysql_error());
         } else {
-            die("Creation of database $databaseName failed: " . mysql_error());
+            print("Creation of database $databaseName worked. <br/>");
         }
     }
 }
-    $connectTable = mysql_query("SHOW TABLES;");
-    if (!$connectTable) {
-        print mysql_error();
-    }
+$connectTable = mysql_query("SHOW TABLES;");
+if (!$connectTable) {
+    print mysql_error();
+}
 
-    $tableCount = mysql_num_rows($connectTable);
-    if ($tableCount == 0) {
-        mysql_query("CREATE TABLE $tableName.`new_table` (
-  `idNotes` INT NOT NULL AUTO_INCREMENT,
-  `topic` VARCHAR(255) NULL,
-  `dateCreated` INT(11) NULL,
-  `dateEdited` INT(11) NULL,
-  `notes` TEXT NULL,
-  PRIMARY KEY (`idNotes`));
-");
-    }
-    $db = mysql_select_db("notebook", $connection) or die("Could not connect to table {$tableName}");
+$tableCount = mysql_num_rows($connectTable);
+if ($tableCount == 0) {
+    $createTableSchema = mysql_query("CREATE TABLE {$tableName} (idNotes INT NOT NULL AUTO_INCREMENT,topic VARCHAR(255) NULL,dateCreated INT(11) NULL,dateEdited INT(11) NULL,notes TEXT NULL, PRIMARY KEY (idNotes));") or die(print mysql_error());
+    print "Created table schema {$tableName}.";
+}
+$db = mysql_select_db("notebook", $connection) or die("Could not connect to table {$tableName}");
 
-    function createTableStart() {
+function createTableStart() {
 
-        print "<table id='mytable' cellspacing='0' border='1px' summary='All topics list'>
+    print "<table id='mytable' cellspacing='0' border='1px' summary='All topics list'>
                   <tr>
                    <th scope='col' width='400px'>Topic</th>
                    <th scope='col' width='140px'>Last Edit</th>
                   </tr>
                ";
-    }
+}
 
-    function convertUnixTime($time) {
-        return gmdate("H:i:s Y-m-d ", $time);
-    }
+function convertUnixTime($time) {
+    return gmdate("H:i:s Y-m-d ", $time);
+}
 
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+include('dbConnect.php');
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,21 +15,24 @@ and open the template in the editor.
     <body>
         <h2>Notebook</h2>
         <?php
-        include('dbConnect.php');
         session_start();
         createTableStart();
-        
+
         $query = "SELECT * FROM notes ORDER BY topic";
-        $result = mysql_query($query);
-        while ($row = mysql_fetch_array($result, MYSQLI_ASSOC)) {
-            echo "<tr>";
-            echo "<td>{$row['topic']}</td>";
-            $formTime = convertUnixTime($row['dateEdited']);
-            echo "<td>{$formTime}</td>";
-            echo "</tr>";
+        $result = mysql_query($query, $connection);
+        if (!$result) {
+            print "no rows";
+        } else {
+            while ($row = mysql_fetch_array($result, MYSQLI_ASSOC)) {
+                echo "<tr>";
+                echo "<td>{$row['topic']}</td>";
+                $formTime = convertUnixTime($row['dateEdited']);
+                echo "<td>{$formTime}</td>";
+                echo "</tr>";
+            }
         }
         echo "</table>";
-        
+
         print "<form method='link' action='addNew.php'>
                <input type='submit' value='New Note'/>
                </form>";
