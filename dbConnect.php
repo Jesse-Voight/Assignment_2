88dd7254798ad14842c9ab266f86dc5ab1b96447
Jesse-Voight <24051java1@gmail.com>
2014-05-08 14:12:31 +1200

    Added functionality of creating a database and table if one does not exist and various checks.

--- dbConnect.php
+++ dbConnect.php
@@ -1,20 +1,56 @@
 <?php
 
-$connection = mysql_connect("localhost", "assignment2", "winning")
-        or die("Database connection could not be established");
-$db = mysql_select_db("notebook", $connection)
-        or die("Could not connect to table 'notes'");
+#declare varriables
+$tableName = "notes";
+$databaseName = "notebook";
+#
 
-function createTableStart() {
+$connection = mysql_connect("localhost", "assignment2", "winning") or die("Database connection to MySql server could not be established");
 
-    print "<table id='mytable' cellspacing='0' border='1px' summary='All topics list'>
+$dbCheck = mysql_select_db($databaseName, $connection);
+
+if (!$dbCheck) {
+    $createDB = mysql_query("CREATE DATABASE $databaseName", $connection);
+    if ($createDB) {
+        print("Created initial database $databaseName");
+        $dbCheck = mysql_select_db($databaseName, $connection);
+        if (!$dbCheck) {
+            die("Couldnt open $databaseName - fatal error" . mysql_error());
+        } else {
+            die("Creation of database $databaseName failed: " . mysql_error());
+        }
+    }
+}
+    $connectTable = mysql_query("SHOW TABLES;");
+    if (!$connectTable) {
+        print mysql_error();
+    }
+
+    $tableCount = mysql_num_rows($connectTable);
+    if ($tableCount == 0) {
+        mysql_query("CREATE TABLE $tableName.`new_table` (
+  `idNotes` INT NOT NULL AUTO_INCREMENT,
+  `topic` VARCHAR(255) NULL,
+  `dateCreated` INT(11) NULL,
+  `dateEdited` INT(11) NULL,
+  `notes` TEXT NULL,
+  PRIMARY KEY (`idNotes`));
+");
+    }
+    $db = mysql_select_db("notebook", $connection) or die("Could not connect to table {$tableName}");
+
+    function createTableStart() {
+
+        print "<table id='mytable' cellspacing='0' border='1px' summary='All topics list'>
                   <tr>
                    <th scope='col' width='400px'>Topic</th>
                    <th scope='col' width='140px'>Last Edit</th>
                   </tr>
                ";
-}
-function convertUnixTime($time) {
-    return gmdate("H:i:s Y-m-d ", $time);
-}
+    }
+
+    function convertUnixTime($time) {
+        return gmdate("H:i:s Y-m-d ", $time);
+    }
+
 ?>
